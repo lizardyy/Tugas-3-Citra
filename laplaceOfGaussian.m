e596a6a0406173d227ccbee1248d058c30af8618
Mahesa Lizardy <88304455+lizardyy@users.noreply.github.com>
2023-10-29 19:43:04 +0700

    log and sobel

--- laplaceOfGaussian.m
+++ laplaceOfGaussian.m
@@ -1,4 +1,14 @@
 % Pendeteksian tepi dengan dengan operator laplace of gaussian
-function result = laplaceOfGaussian(imagedata, N)
+function result = laplaceOfGaussian(grayImage, threshold)
+
+    logMask = [0, -1, 0; -1, 4, -1; 0, -1, 0];
+    result = conv2(double(grayImage), double(logMask), 'same');
     
+    % thresholding to binary image
+    result = result > threshold;
+    
+    % Remove pixel tapi to 0 
+    result([1 end], :) = 0; % Top and bottom rows
+    result(:, [1 end]) = 0; % Left and right columns
+    disp(result)
 end
--- sobel.m
+++ sobel.m
@@ -1,4 +1,21 @@
-% Pendeteksian tepi dengan dengan operator sobel
-function result = sobel(imagedata, N)
+% Pendeteksian tepi dengan dengan operator laplace of gaussian
+function result = sobel(grayImage, threshold)
+
+    % Sobel Kernels
+    sobelHorizontal = [-1, 0, 1; -2, 0, 2; -1, 0, 1];
+    sobelVertical = [1, 2, 1; 0, 0, 0; -1, -2, -1];
+
+    % Convolve image with Sobel kernels
+    gradientX = conv2(double(grayImage), double(sobelHorizontal), 'same');
+    gradientY = conv2(double(grayImage), double(sobelVertical), 'same');
+
+    % Gradient Magnitude and Direction
+    result = uint8(sqrt(gradientX.^2 + gradientY.^2));
+    result = result > threshold;
     
+    % Remove pixel tapi to 0 
+    result([1 end], :) = 0; % Top and bottom rows
+    result(:, [1 end]) = 0; % Left and right columns
+    disp(result)
 end
+
